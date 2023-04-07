bac00a107f76e173dd00ae89398c0e2bf5710eb6
RubenCarreira99 <ruben99carreira@gmail.com>
2023-04-07 14:28:50 +0100

    'adesfsdfsdqwerty'

--- teste/teste/prod_settings.py
+++
@@ -1,9 +0,0 @@
-from settings import *
-import os
-
-DEBUG = False
-
-SECRET_KEY = os.environ['SECRET_KEY']
-
-# SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']

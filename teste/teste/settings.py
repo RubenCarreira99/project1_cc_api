00d01603a58d5b543dff5fde7506966cf93b0fc6
RubenCarreira99 <ruben99carreira@gmail.com>
2023-04-11 19:00:52 +0100

    'adesfsdfsdqwerty'

--- teste/teste/settings.py
+++ teste/teste/settings.py
@@ -133,14 +133,14 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql',
         #'NAME': 'postgres',
-        'NAME': os.getenv('DB_NAME', 'dbproj1_pgres_aws'),#'dbproj1_pgres_aws', #os.environ.get('DB_NAME'), 
+        'NAME': os.getenv('DB_NAME'),#, 'dbproj1_pgres_aws'),#'dbproj1_pgres_aws', #os.environ.get('DB_NAME'), 
         #'USER': 'masteruser',
         #'PASSWORD': '12345678',
         #'HOST': 'teste-db.czenacwbzgqz.eu-north-1.rds.amazonaws.com',
-        'USER': os.getenv('DB_USER', 'useraws'),#os.environ.get('DB_USER'), #'useraws',
-        'PASSWORD': os.getenv('DB_PASSWORD', 'Qwerty1234'), #os.environ.get('DB_PASSWORD'), #'Qwerty1234',
-        'HOST': os.getenv('DATABASE_URL', 'dbproj1aws.cnprtk3bvxit.eu-west-2.rds.amazonaws.com'),#os.environ.get('DATABASE_URL'), #'dbproj1aws.cnprtk3bvxit.eu-west-2.rds.amazonaws.com',
-        'PORT': os.getenv('DB_PORT', '5432'),#os.environ.get('DB_PORT'), #'5432',
+        'USER': os.getenv('DB_USER'), #'useraws'),#os.environ.get('DB_USER'), #'useraws',
+        'PASSWORD': os.getenv('DB_PASSWORD'),# 'Qwerty1234'), #os.environ.get('DB_PASSWORD'), #'Qwerty1234',
+        'HOST': os.getenv('DATABASE_URL'), # 'dbproj1aws.cnprtk3bvxit.eu-west-2.rds.amazonaws.com'),#os.environ.get('DATABASE_URL'), #'dbproj1aws.cnprtk3bvxit.eu-west-2.rds.amazonaws.com',
+        'PORT': os.getenv('DB_PORT'),# '5432'),#os.environ.get('DB_PORT'), #'5432',
     }
 }
 

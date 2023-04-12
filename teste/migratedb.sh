aecc2a24d29e4eabce3a95d403fa9d3174f75944
RubenCarreira99 <ruben99carreira@gmail.com>
2023-04-12 01:05:16 +0100

    qwer

--- teste/migratedb.sh
+++ teste/migratedb.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-python manage.py migrate --no-input
+DJANGO_SUPERUSER_USERNAME=admin
+DJANGO_SUPERUSER_EMAIL=admin@example.com
+DJANGO_SUPERUSER_PASSWORD=superuserpassword
 
-RUN echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
+python manage.py migrate --no-input
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+
+python manage.py runserver 0.0.0.0:8000
